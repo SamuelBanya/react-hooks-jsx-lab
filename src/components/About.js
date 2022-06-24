b2b2c0551eb1983a70f947d58818f48debb7afee
Samuel Banya <sbanya@fastmail.com>
2022-06-24 18:34:34 -0400

    Adding latest revisions to assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>A tech support analyst by day, an Emacs wielding developer by night</p>
+    <img alt="I made this" src={image}/>
+    </div>;
 }
 
 export default About;
